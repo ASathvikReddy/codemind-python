875fecf5e6cbacfc620aa82c799944c8c2877e8e
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-05-24 06:11:03 +0530

    def pal(n):     x=n     rev=0     while x:        r=x%10        rev=(rev*10)+r        x=x//10     if(rev==n):         return 1     else:         return 0
    a=int(input())
    b=int(input())
    for i in range(a,b+1):
        if(pal(i)):
            print(i,end=" ")

---
+++ Program_to_print_all_palindromes_in_a_given_range_.py
@@ -0,0 +1,17 @@
+def pal(n):
+    x=n
+    rev=0
+    while x:
+       r=x%10
+       rev=(rev*10)+r
+       x=x//10
+    if(rev==n):
+        return 1
+    else:
+        return 0
+        
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if(pal(i)):
+        print(i,end=" ")
