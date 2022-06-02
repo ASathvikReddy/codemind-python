24e9d6390bebdf799aca57ed917dd69e8ba232cd
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-06-02 21:24:46 +0530

    def prime(n):     for i in range(2,(n//2)+1):         if n%i==0:             return False     return True n=int(input()) a=list(map(int,input().split())) b=int(input()) c=0 for i in a:     if i!=1:         if prime(i) and i<=b:             c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,14 @@
+def prime(n):
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+a=list(map(int,input().split()))
+b=int(input())
+c=0
+for i in a:
+    if i!=1:
+        if prime(i) and i<=b:
+            c+=1
+print(c)
