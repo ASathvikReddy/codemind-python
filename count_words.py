97a3c7bae1947821a9483518ca0448f0c4f2b2e7
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-06-28 19:27:15 +0530

    n=input() n=n.lower() n=n.split(' ') c=0 for i in range(len(n)):     a=''.join(n[i])     if a[0] in 'aeiou' and a[len(a)-1] not in 'aeiou':         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -4,6 +4,6 @@ n=n.split(' ')
 c=0
 for i in range(len(n)):
     a=''.join(n[i])
-    if a[0] in 'aeiou' and a[len(a)-1] in 'bcdfghjklmnpqrstvwxyz':
+    if a[0] in 'aeiou' and a[len(a)-1] not in 'aeiou':
         c+=1
 print(c)
