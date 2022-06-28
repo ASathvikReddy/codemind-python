53acd6b0e66fe64580d84db6cfef8e62cedbed21
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-06-28 18:40:18 +0530

    s=list(map(str,input().split())) c=0 a=[] for i in s:     for j in i:         if j in "aeiou":             c+=1         elif j in "AEIOU":             c+=1     a.append(c)     c=0 print(max(a))

---
+++ maximum_vowel_count.py
@@ -0,0 +1,12 @@
+s=list(map(str,input().split()))
+c=0
+a=[]
+for i in s:
+    for j in i:
+        if j in "aeiou":
+            c+=1
+        elif j in "AEIOU":
+            c+=1
+    a.append(c)
+    c=0
+print(max(a))
