3869b4ac77788b06f61b812e2168ee79928d569e
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-07-01 18:09:14 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True n=int(input()) temp=n for i in range(n,2,-1):     if prime(i):         p=i         break while temp!=0:     if prime(temp):         q=temp         break     temp+=1 if (n-p)<(q-n):     print(abs(n-p)) elif (n-p)==(q-n):     print(abs(n-p)) else:     print(abs(q-n))

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,24 +1,26 @@
 def prime(n):
-    for i in range(2,(n//2)+1):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return False
     else:
         return True
 n=int(input())
-m=n
-if n==1 or n==2 or n==3 or n==5 or n==7:
-    print("0")
+temp=n
+for i in range(n,2,-1):
+    if prime(i):
+        p=i
+        break
+while temp!=0:
+    if prime(temp):
+        q=temp
+        break
+    temp+=1
+if (n-p)<(q-n):
+    print(abs(n-p))
+elif (n-p)==(q-n):
+    print(abs(n-p))
 else:
-    for i in range(n,2,-1):
-        if prime(i):
-            a=i
-            break
-    while m!=0:
-        if prime(m):
-            b=m
-            break
-        m+=1
-    if b-n>n-a or b-n==n-a:
-        print(abs(n-a))
-    else:
-        print(abs(b-n))
+    print(abs(q-n))
+            
