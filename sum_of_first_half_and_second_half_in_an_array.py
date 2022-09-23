8d0670063b2eaafab58755ebbe29978ef8b89553
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-09-23 18:08:39 +0530

    n=int(input()) a=list(map(int,input().split())) s=s1=0 for i in range(0,(n//2)):     s+=a[i] for i in range((n//2),n):     s1+=a[i] print(s) print(s1)

--- sum_of_first_half_and_second_half_in_an_array.py
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -1,10 +1,9 @@
 n=int(input())
 a=list(map(int,input().split()))
-s1=0
-s2=0
-for i in range(0,(n//2)+1):
-    s1+=i
-for i in range((n//2)+1,n+1):
-    s2+=i
+s=s1=0
+for i in range(0,(n//2)):
+    s+=a[i]
+for i in range((n//2),n):
+    s1+=a[i]
+print(s)
 print(s1)
-print(s2)
