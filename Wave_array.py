3e11a319546a3bfdf89e7bcaca9e97048f8289f3
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-09-23 15:21:59 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 if a[0]>a[1]:     for i in range(n-1):         if i%2==0 and a[i]>a[i+1]:             c+=1         elif i%2 and a[i]<a[i+1]:             c+=1     if c==n-1:         print("yes")     else:         print("no") else:     for i in range(n-1):         if i%2==0 and a[i]<a[i+1]:             c+=1         elif i%2 and a[i]>a[i+1]:             c+=1     if c==n-1:         print("yes")     else:         print("no")

--- Wave_array.py
+++ Wave_array.py
@@ -1,23 +1,23 @@
-n = int(input())
-arr = list(map(int,input().split()))
-count = 0
-if arr[0]>arr[1]:
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+if a[0]>a[1]:
     for i in range(n-1):
-        if i%2==0 and arr[i]>arr[i+1]:
-            count+=1
-        elif i%2 and arr[i]<arr[i+1]:
-            count+=1
-    if count==(n-1):
+        if i%2==0 and a[i]>a[i+1]:
+            c+=1
+        elif i%2 and a[i]<a[i+1]:
+            c+=1
+    if c==n-1:
         print("yes")
     else:
         print("no")
 else:
     for i in range(n-1):
-        if i%2==0 and arr[i]<arr[i+1]:
-            count+=1
-        elif i%2 and arr[i]>arr[i+1]:
-            count+=1
-    if count==n-1:
+        if i%2==0 and a[i]<a[i+1]:
+            c+=1
+        elif i%2 and a[i]>a[i+1]:
+            c+=1
+    if c==n-1:
         print("yes")
     else:
         print("no")
