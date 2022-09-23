ec16462149d013eee02c0a761f9a1127de7e19d8
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-09-23 16:54:34 +0530

    n = int(input()) arr = list(map(int,input().split())) e = [] o = [] for i in range(0,n):     if arr[i]%2==0:         e.append(arr[i])     else:         o.append(arr[i]) i = 0 j = 0 while i<len(e) or j<len(o):     if j<len(o):         print(o[j],end=" ")         j+=1     if i<len(e):         print(e[i],end=" ")         i+=1 if n%2!=0:     print("0")

--- odd_even_pairs.py
+++ odd_even_pairs.py
@@ -9,12 +9,12 @@ for i in range(0,n):
         o.append(arr[i])
 i = 0
 j = 0
-while i<len(o) or j<len(e):
-    if i<len(o):
-        print(o[i],end=" ")
-        i+=1
-    if j<len(e):
-        print(e[j],end=" ")
+while i<len(e) or j<len(o):
+    if j<len(o):
+        print(o[j],end=" ")
         j+=1
+    if i<len(e):
+        print(e[i],end=" ")
+        i+=1
 if n%2!=0:
     print("0")
